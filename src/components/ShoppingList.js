7c8bcdf0dbb0d52d71825e456b1cf84191c494fa
Kevin Diesenberg <kdiesenb@gmail.com>
2022-11-26 21:57:47 -0500

    search and filter display almost working. need to figure out how to reset the itemstodisplay between switching filter/search

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,32 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (search.toLowerCase() === item.name.toLowerCase()) {
+      return item.name
+    }
+    else {
+      if (item.category === selectedCategory) {
+        return selectedCategory;
+      }
+    }
   });
+
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        search={search}
+        onSearchChange={setSearch}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
